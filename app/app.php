91599e2bf91807e8389631b64ab5f048d92adaac
Abeer Khakwani & Bojana Skarich <student@epicodus.com>
2015-03-23 15:33:11 -0700

    Add app.php. Add three twig files. Add routes to app.php.

--- app/app.php
+++ app/app.php
@@ -16,10 +16,76 @@
     use Symfony\Component\HttpFoundation\Request;
     Request::enableHttpMethodParameterOverride();
 
-    //load homepage and display all the categories from the database
-    $app-get("/", function() use ($app) {
-        return $app['twig']->render('index.twig', array('categories' => Category::getAll()));
+//#######################################Home Routes#######################################
+//#######################################Home Routes#######################################
+
+//#######################################Home Routes#######################################
+
+    //Load homepage and display two links to categories and tasks
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('home.twig');
 
     });
 
+//###################################Categories Routes#######################################
+//###################################Categories Routes#######################################
+//###################################Categories Routes#######################################
+//###################################Categories Routes#######################################
+    //Displays all the categories from the database when user clicks categories link
+    $app->get("/categories", function() use ($app) {
+        return $app['twig']->render('DisplaysCategoriesAndForm.twig', array('category_array' => Category::getAll()));
+
+    });
+
+
+    //Posts new categories based on user input from form
+    $app->post("/categories", function() use ($app) {
+        $category = new Category($_POST['name']);
+        $category->save();
+        return $app['twig']->render('DisplaysCategoriesAndForm.twig', array('category_array' => Category::getAll()));
+
+    });
+
+    $app->post("/delete_categories", function() use ($app) {
+        Category::deleteAll();
+        return $app['twig']->render('DisplaysCategoriesAndForm.twig', array('category_array' => Category::getAll()));
+
+    });
+
+
+//#################################Tasks Routes########################################
+//#################################Tasks  Routes####################################
+//#################################Tasks Routes########################################
+//###################################Tasks Routes########################################
+//###################################Tasks Routes########################################
+//###################################Tasks Routes########################################
+
+    $app->get("/tasks", function() use ($app) {
+        return $app['twig']->render('view_tasks.twig', array('tasks' => Task::getAll()));
+
+    });
+
+    //Posts new tasks based on user input from form
+    $app->post("/tasks", function() use ($app) {
+        $new_task = new Task($_POST['description']);
+        $new_task->save();
+        return $app['twig']->render('view_tasks.twig', array('tasks' => Task::getAll()));
+
+
+    });
+
+
+    $app->post("/delete_tasks", function() use ($app) {
+        Task::deleteAll();
+        return $app['twig']->render('view_tasks.twig', array('tasks' => Task::getAll()));
+
+    });
+
+
+
+
+
+
+
+    return $app;
 ?>
